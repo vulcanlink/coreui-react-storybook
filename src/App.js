6775dbf12b08725e8ab9050e7077445ee661b350
Leo Vigna <leo@joinsaturn.com>
2020-05-10 01:37:34 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { CButton } from '@coreui/react'
+
+export const Text = () => <CButton color={'primary'}>Primary</CButton>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+                <Text />
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </header>
+        </div >
+    );
 }
 
 export default App;
---
+++ src/stories/2-Base.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CButton } from '@coreui/react'
+
+export default {
+    title: 'CButton',
+    component: CButton,
+};
+
+export const CButtonPrimary = () => <CButton color='primary'>Primary</CButton>;
+
