cb7c27dc62422b4390d6c6ee36c3495120f34a44
Leo Vigna <leo@joinsaturn.com>
2020-05-12 12:41:37 +0200

    Updates

---
+++ main.23ee617c7924daea8b84.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{472:function(n,o,p){p(473),p(616),p(1262),p(1368),p(1373),p(1376),n.exports=p(1381)},535:function(n,o){}},[[472,1,2]]]);
---
+++ precache-manifest.2983ae75da4037d63e0fc88c1158c7e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f117e5564fdd00b159c90b45a1f541d1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.39fa9d9096bb01a45366.bundle.js"
+  },
+  {
+    "url": "runtime~main.39fa9d9096bb01a45366.bundle.js"
+  },
+  {
+    "revision": "d288a33c9d9cd7070853",
+    "url": "static/css/vendors~main.2813a45c.chunk.css"
+  },
+  {
+    "url": "vendors~main.39fa9d9096bb01a45366.bundle.js"
+  },
+  {
+    "url": "vendors~main.39fa9d9096bb01a45366.bundle.js.LICENSE.txt"
+  }
+]);
